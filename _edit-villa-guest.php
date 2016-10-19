05bbcd702328aa364710385b7c934d972adab390
pomchai <pomchai@hotmail.com>
2016-10-19 10:29:52 +0700

    Update _edit-villa-guest.php
    add /*zzzz*/ 3 line and delete /*bbbb*/ 4 line

--- _edit-villa-guest.php
+++ _edit-villa-guest.php
@@ -5,11 +5,9 @@ error_reporting( error_reporting() & ~E_NOTICE );
 /*aaaaaaaaaaaaa */
 /*bbbbbbbbbbbbb */
 
-/*bbbbbbbbbbbbb */
-/*bbbbbbbbbbbbb */
-
-/*bbbbbbbbbbbbb */
-/*bbbbbbbbbbbbb */
+/*zzzzzzz*/
+/*zzzzzzz*/
+/*zzzzzzz*/
 session_start();
 include "../inc/config.inc.php";
 //echo "$_SESSION[m_login]<br>$_SESSION[m_id]";
